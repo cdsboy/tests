d7e5b6416f2f063d3533407214813ba0cfbf1908
Brandon Bowlby <cdsboy@cdsboy.com>
2012-02-03 14:40:44 -0600

    sleep and repeat

--- awesome/bckswp.rb
+++ awesome/bckswp.rb
@@ -2,17 +2,24 @@
 
 BACKGROUND_DIR = "/home/cdsboy/Pictures/Backgrounds"
 
-# make sure file exists before reading it
-File.open(BACKGROUND_DIR + "/.curback", 'a')
-cur_background = IO.readlines(BACKGROUND_DIR + "/.curback")[0]
-
-Dir.chdir(BACKGROUND_DIR)
-files = Dir.glob('*')
-
-new_background = ""
-until new_background != cur_background and new_background != ""
-  new_background = files[rand(files.length)]
+def set_wallpaper
+  # make sure file exists before reading it
+  File.open(BACKGROUND_DIR + "/.curback", 'a')
+  cur_background = IO.readlines(BACKGROUND_DIR + "/.curback")[0]
+  
+  Dir.chdir(BACKGROUND_DIR)
+  files = Dir.glob('*')
+  
+  new_background = ""
+  until new_background != cur_background and new_background != ""
+    new_background = files[rand(files.length)]
+  end
+  
+  File.open(BACKGROUND_DIR + "/.curback", 'w') {|f| f.write(new_background) }
+  system "awsetbg %s/%s" % [BACKGROUND_DIR, new_background]
 end
 
-File.open(BACKGROUND_DIR + "/.curback", 'w') {|f| f.write(new_background) }
-system "awsetbg %s/%s" % [BACKGROUND_DIR, new_background]
+while true
+  set_wallpaper
+  sleep 60*15
+end
