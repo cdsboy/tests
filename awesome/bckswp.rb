52484abee1a814cd4a79bf967b31b0b5bff75b7d
Brandon Bowlby <cdsboy@shortboy.(none)>
2012-02-03 12:40:12 -0600

    awesome background swaping script

---
+++ awesome/bckswp.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+BACKGROUND_DIR = "/home/cdsboy/Pictures/Backgrounds"
+
+# make sure file exists before reading it
+File.open(BACKGROUND_DIR + "/.curback", 'a')
+cur_background = IO.readlines(BACKGROUND_DIR + "/.curback")[0]
+
+Dir.chdir(BACKGROUND_DIR)
+files = Dir.glob('*')
+
+new_background = ""
+until new_background != cur_background and new_background != ""
+  new_background = files[rand(files.length)]
+end
+
+File.open(BACKGROUND_DIR + "/.curback", 'w') {|f| f.write(new_background) }
+system "awsetbg %s/%s" % [BACKGROUND_DIR, new_background]
