907b818e2fe194f6bca23f4153f216933eeaeb4c
Brandon <cdsboy@ns4003976.ip-142-4-212.net>
2012-10-11 04:26:57 +0000

    new import and working ajax call

--- anime/import.py
+++ anime/import.py
@@ -6,46 +6,60 @@ import shows
 wb = load_workbook(filename = r'survey.xlsx')
 ws = wb.worksheets[0]
 
-conn = psycopg2.connect("dbname=recs user=postgres")
+conn = psycopg2.connect("host=localhost dbname=recs user=cdsboy")
 
 cur = conn.cursor()
 
 def get_or_create_show(name):
-  try:
+  cur.execute("select show_id from shows where show_name = %s", (name,))
+  result = cur.fetchone()
+  if not result:
     cur.execute("insert into shows(show_name) values(%s) returning show_id",
                 (name,))
-  except: #lol blanket excepts are bad mmmk
-    cur.execute("select show_id from shows where show_name = %s" (name,))
-  return cur.fetchone()[0]
+    result = cur.fetchone()
+  return result[0]
 
 def get_or_create_genre(name):
-  try:
+  cur.execute("select genre_id from genres where genre_name = %s", (name,))
+  result = cur.fetchone()
+  if not result:
     cur.execute("insert into genres(genre_name) values(%s) returning genre_id",
                 (name,))
-  except:
-    cur.execute("select genre_id from genres where genre_name = %s", (name,))
-  return cur.fetchone()[0]
+    result = cur.fetchone()
+  return result[0]
 
 def get_or_create_demo(name):
-  try:
+  cur.execute("select demo_id from demographics where demo_name = %s", (name,))
+  result = cur.fetchone()
+  if not result:
     cur.execute("insert into demographics(demo_name) values(%s) returning "
                 "demo_id", (name,))
-  except:
-    cur.execute("select demo_id from demographics where demo_name = %s",
-                (name,))
-  return cur.fetchone()[0]
+    result = cur.fetchone()
+  return result[0]
+
+GENRES = ["Action", "Comedy", "Drama", "Dystopian", "Ecchi", "Fantasy", "Game",
+          "Harem", "Hentai", "Horror", "Mahou Shoujo", "Mecha", "Music",
+          "Mystery", "Romance", "Sci-Fi", "Slice of life", "Sports"]
 
 for row in ws.rows:
   try:
     show_name = shows.shows[row[12].value.lower()]
-  except:
+    if not show_name:
+      continue
+  except KeyError:
     continue
+  
+  genre = row[7].value
+  least = row[19].value
+  if not genre in GENRES or not least in GENRES:
+    continue
+
   cur.execute("insert into entries(sex, age, show_id, demo_id, fav_genre, "
               "least_genre) values(%s, %s, %s, %s, %s, %s)", (row[1].value,
                 row[2].value, get_or_create_show(show_name),
                 get_or_create_demo(row[6].value),
-                get_or_create_genre(row[7].value),
-                get_or_create_genre(row[19].value)))
+                get_or_create_genre(genre),
+                get_or_create_genre(least)))
 
 conn.commit()
 cur.close()
---
+++ anime/server.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/ajax/search')
+def search():
+  print "searching"
+  conn = psycopg2.connect("host=localhost dbname=recs user=cdsboy")
+  cur = conn.cursor()
+
+  genres = request.args.get('genres', None, type=list)
+  genres = tuple([int(genre) for genre in genres if genre != u','])
+  leasts = request.args.get('leasts', None)
+  leasts = tuple([int(genre) for genre in leasts if genre != u','])
+  sex = request.args.get('sex', None)
+  age = request.args.get('age', None)
+  demo = request.args.get('demo', None, type=int)
+
+  qry = ("select show.show_name, count(entry.entry_id) "
+         "from entries entry, shows show "
+         "where entry.show_id = show.show_id ")
+  args = []
+
+  if genres:
+    qry += "and entry.fav_genre in %s "
+    args.append(genres)
+  if leasts:
+    qry += "and entry.least_genre in %s "
+    args.append(leasts)
+  if age:
+    qry += "and entry.age = %s "
+    args.append(age)
+  if sex:
+    qry += "and entry.sex = %s "
+    args.append(sex)
+  if demo:
+    qry += "and entry.demo_id = %s "
+    args.append(demo)
+
+  qry += "group by show.show_name order by count desc"
+
+  cur.execute(qry, args)
+  result = cur.fetchall()
+
+  cur.close()
+  conn.close()
+
+  return jsonify(result=result)
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host='0.0.0.0')
